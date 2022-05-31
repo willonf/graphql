5d7e5c7ecf88c3d5e850e77f3b8622754b3b1db4
Willon Ferreira <contatowillon@gmail.com>
2022-05-31 16:48:03 -0400

    Fix post resolver return

--- src/graphql/post/resolvers.ts
+++ src/graphql/post/resolvers.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const post = (parent: any, arg: any, context: any, info: any) => {
-    const id = "2"
-    const URL = 'http://localhost:3000/posts/'.concat(id).concat("/");
-    return axios.get(URL, {headers: {Accept: 'application/json'}})
+const post = async (parent: any, arg: any, context: any, info: any) => {
+    const URL = 'http://localhost:3000/posts/'.concat(arg.id).concat("/");
+    const {data, status} = await axios.get(URL, {
+        headers: {Accept: 'application/json'},
+    });
+    return data
 };
 
 const posts = async (parent: any, arg: any, context: any, info: any) => {
